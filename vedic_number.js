a4138262cc3d76dbd7b68e1109a6602985a64b50
komal <komal0385.becse24@chitkara.edu.in>
2025-04-22 00:01:56 +0530

    changes in code results and numbers

--- vedic_number.js
+++ vedic_number.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     calculateButton.addEventListener('click', function() {
         const numbersInput = document.getElementById('numbers').value;
         const numbers = numbersInput.split(',').map(Number);
-        const result = numbers.reduce((acc, num) => acc + num, 0); // Example: sum of numbers in it
+        const result = numbers.reduce((acc, num) => acc + num, 0); // Example: sum of numbers
         resultDisplay.textContent = 'Result: ' + result;
-    }
+    });
+});
